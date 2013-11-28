38595db5ee872ef21a1fba592e9f120c9a22eba8
admin <ikhsan.zone@gmail.com>
2013-11-28 16:36:05 +0700

    upload file home

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+include 'header.php';
+?>
+	<div class="container">
+		<div class="row">
+			
+			<div class="span9">
+				<div class="hero-unit">
+                	<p align="center"><img src="img/logo.png" height="150" width="520" align="middle"></p>
+				</div>
+                <div class="hero-unit">
+                	<center>
+                	<h2>Silakan Pilih Kategori</h2>
+                        <ul class="kategori">
+                            <li><a href="kategori1.php" class="btn btn-primary btn-large"><span>Kategori 1</span></a></li>
+                            <li><a href="#" class="btn btn-primary btn-large"><span>Kategori 2</span></a></li>
+                            <li><a href="#" class="btn btn-primary btn-large"><span>Kategori 3</span></a></li>
+                        </ul>
+                    </center>
+				</div>
+
+				
+			</div>
+		</div>
+	</div>
+	
+	<hr />
+	<?php
+	include 'footer.php';
+	?>
