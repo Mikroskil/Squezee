4b3cba48ad2f0597426504d5e403b27abe246e0a
admin <ikhsan.zone@gmail.com>
2013-11-28 16:32:31 +0700

    cara memilih

---
+++ cara.php
@@ -0,0 +1,77 @@
+<?php
+ob_start();
+error_reporting(0);
+include_once 'koneksi.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+	<title><?php echo $judul; ?></title>
+
+	<!-- Included Bootstrap CSS Files -->
+	<link rel="stylesheet" href="./js/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="./js/bootstrap/css/bootstrap-responsive.min.css" />
+	
+	<!-- Includes FontAwesome -->
+	<link rel="stylesheet" href="./css/font-awesome/css/font-awesome.min.css" />
+
+	<!-- Css -->	
+	<link rel="stylesheet" href="./css/style.css" />
+    <link rel="shortcut icon" href="img/favicon.png">
+
+</head>
+<body>
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<button class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse" type="button">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="brand" href="index.php">E-Voting</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+                        <li><a href="cara.php">Cara memilih</a></li>
+                        <li><a href="kandidat.php">Kandidat</a></li>
+                        <li><a href="login.php">Login</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			
+			<div class="span9">
+				<div class="hero-unit">
+                	<p align="center"><img src="img/logo.png" height="150" width="520" align="middle"></p>
+				</div>
+                <div class="hero-unit">
+                	<center>
+                	<h2>Cara Memilih</h2>
+                    </center>
+                        <p>1. Silakan Login menggunakan akun anda.</p>
+                        <p>2. Pilih Kategori yang ingin anda pilih dan klik tombol <b>PILIH.</b></p>
+                        <p>3. Pilih calon kandidat anda.</p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                    
+				</div>
+
+				
+			</div>
+		</div>
+	</div>
+	
+	<hr />
+	<?php
+	include 'footer.php';
+	?>
