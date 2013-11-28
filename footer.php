62b53aa5983a6a784eea8e8e173829565d504026
admin <ikhsan.zone@gmail.com>
2013-11-28 16:33:16 +0700

    upload file footer

---
+++ footer.php
@@ -0,0 +1,37 @@
+
+	<footer id="footer">
+		<div class="container">
+        	<!--
+			<div class="row">
+				<div class="span4">
+					<h4>Info</h4>
+					<p><img src="img/uisu.jpg" height="100" width="150"></p>
+				</div> 
+
+				<div class="span4">
+					<h4>Location and Contacts</h4>
+					<p><i class="icon-map-marker"></i>&nbsp;I do not Know Avenue, A City</p>
+					<p><i class="icon-phone"></i>&nbsp;Phone: 234 739.126.72</p>
+					<p><i class="icon-print"></i>&nbsp;Fax: 213 123.12.090</p>
+					<p><i class="icon-envelope"></i>&nbsp;Email: info@mydomain.com</p>
+					<p><i class="icon-globe"></i>&nbsp;Web: http://www.mydomain.com</p>
+				</div>
+
+			</div>
+            -->
+
+			<div class="row">
+				<div class="span6">
+					<p>&copy; Copyright 2013.&nbsp;<a href="http://www.mikroskil.ac.id/">MIKROSKIL</a> - All Rights Reserved</p>
+				</div>
+				
+			</div>
+		</div>
+	</footer>	
+
+	<script src="./js/jquery-1.10.0.min.js"></script>
+	<script src="./js/bootstrap/js/bootstrap.min.js"></script>
+	<script src="./js/holder.js"></script>
+	<script src="./js/script.js"></script>
+</body>
+</html>
